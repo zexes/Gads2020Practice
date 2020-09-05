8d5972c49f3f9ee4f4fd63d99e0734abe8511c09
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-05 03:55:40 +0100

    implemented submit interface. closes #6

--- app/src/main/java/com/zikozee/LeaderBoardActivity.java
+++ app/src/main/java/com/zikozee/LeaderBoardActivity.java
@@ -4,21 +4,26 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 import com.zikozee.learning.LearningFragment;
 import com.zikozee.skill.SkillFragment;
 
-public class LeaderBoardActivity extends AppCompatActivity {
+public class LeaderBoardActivity extends AppCompatActivity implements View.OnClickListener{
     private Toolbar mToolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,8 @@ public class LeaderBoardActivity extends AppCompatActivity {
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
         //Toolbar
         mToolbar = findViewById(R.id.customToolBar);
         setSupportActionBar(mToolbar);
@@ -51,5 +58,13 @@ public class LeaderBoardActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.submit){
+            Intent intent = new Intent(LeaderBoardActivity.this, SubmissionActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/zikozee/SubmissionActivity.java
@@ -0,0 +1,45 @@
+package com.zikozee;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+
+public class SubmissionActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        ImageView imageView = findViewById(R.id.back_button);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId() == R.id.back_button){
+                    startActivity(new Intent(SubmissionActivity.this, LeaderBoardActivity.class));
+                }
+            }
+        });
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//            }
+//        });
+    }
+}
